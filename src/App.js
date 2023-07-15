8c75ba3d3a047c2798255ddea8c2afd7e27d217d
prashant fulara <139608782+prashant-fulara@users.noreply.github.com>
2023-07-15 06:40:09 +0000

    Update App.js and TaskList.js

--- src/App.js
+++ src/App.js
@@ -6,35 +6,42 @@ let nextId = 3;
 
 export default function App() {
   const [toDos, setToDos] = useState([
-    {id: 0, title: "Buy Milk", done: true},
-    {id: 1, title: "Park Walk", done: false},
-    {id: 2, title: "Eat Fruits", done: false},
-  ])
+    { id: 0, title: "Buy Milk", done: true },
+    { id: 1, title: "Park Walk", done: false },
+    { id: 2, title: "Eat Fruits", done: false },
+  ]);
 
   function handleAddToDo(title) {
-    setToDos(
-      [
-        ...toDos,
-        {
-          id: nextId++,
-          title: title, 
-          done: false}
-      ])
-    }
-  
+    setToDos([
+      ...toDos,
+      {
+        id: nextId++,
+        title: title,
+        done: false,
+      },
+    ]);
+  }
+
   function handleDeleteToDo(todoId) {
+    setToDos(toDos.filter((todo) => todo.id !== todoId));
+  }
+
+  function handleChangeToDo(changedTodo) {
     setToDos(
-        toDos.filter(todo => todo.id !== todoId)
-      )
-    }
+      toDos.map((todo) => {
+        if (todo.id == changedTodo.id) return changedTodo;
+        else return todo;
+      })
+    );
+  }
+
   return (
     <>
-      <AddToDo
-        onAddToDo = {handleAddToDo}
-      />
+      <AddToDo onAddToDo={handleAddToDo} />
       <TaskList
-        toDos = {toDos}
-        onDeleteTodo = {handleDeleteToDo}
+        toDos={toDos}
+        onDeleteTodo={handleDeleteToDo}
+        onChangeTodo={handleChangeToDo}
       />
     </>
   );
